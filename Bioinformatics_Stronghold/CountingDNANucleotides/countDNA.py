f0305de1a0a00df9468316f4de63f81d782d4b28
iaji <ajcardar@gmail.com>
2017-04-20 15:15:13 -0400

    Best answer

--- Bioinformatics_Stronghold/CountingDNANucleotides/countDNA.py
+++ Bioinformatics_Stronghold/CountingDNANucleotides/countDNA.py
@@ -1,23 +1,6 @@
 with open('rosalind_dna.txt', 'r') as f:
     data = f.read()
 
-A = 0
-C = 0
-G = 0
-T = 0
-
-for nucelotide in data:
-    if nucelotide == 'A':
-        A += 1
-    elif nucelotide == 'C':
-        C += 1
-    elif nucelotide == 'G':
-        G += 1
-    elif nucelotide == 'T':
-        T += 1
-    else:
-        print(nucelotide)
-
-result = [A, C, G, T]
-
-print result
+def cnt(data):
+    return data.count('A'), data.count('C'), data.count('G'), data.count('T')
+cnt(data)
