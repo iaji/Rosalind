a54a4f5f2d400cb202ef0a85cb899d1124d762d7
iaji <ajcardar@gmail.com>
2017-04-23 02:31:41 -0400

    WIP 2

--- Bioinformatics_Stronghold/CompGCContent/CompGCContent.py
+++ Bioinformatics_Stronghold/CompGCContent/CompGCContent.py
@@ -1,6 +1,14 @@
 import re
+regexp = re.compile(r'Rosalind.')
+output = ''
+DNAinfo = open('data.txt')
+lines = DNAinfo.readlines()
+StrandPart = []
+FinishedStrand = ''
+#StrandCount = 0
+for line in lines:
+    while not regexp.search(line) and len(line) == 61:
+        for char in line:
+            MostStrand = ''.join(char)
 
-with open('Example.data', 'r') as In_Data:
-    for line in In_Data:
-        if re.findall(('^\<Rosalind_[0-9]+', line):
-            print 'hello'
+print MostStrand
