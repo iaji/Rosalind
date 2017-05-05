73eb72fc4df8a1d11f366930f0add52a904382c5
iaji <ajcardar@gmail.com>
2017-05-05 10:39:26 -0400

    Added function descriptions for the Hamming problem

--- Bioinformatics_Stronghold/CntPointMutations/hammingProb.py
+++ Bioinformatics_Stronghold/CntPointMutations/hammingProb.py
@@ -1,26 +1,30 @@
 def readIn():
+    """ Parses input file into array of lines """
     with open('rosalind_hamm.txt', 'r' ) as f:
         DNA = f.readlines()
+        # print DNA
     return DNA
 
 def compDNA(DNA):
+    """ Takes in array of lines and splits the lines to characters if the character is not a newline """
     DNAArr = []
     for line in DNA:
         for char in line:
             if char != '\n':
                 DNAArr.append(char)
+    # print DNAArr
     return DNAArr
 
 
 
 def main():
+    """ Calls compDNA to append the array of characters to a new Array CompareArr for comparing the same element of two arrays """
     CompareArr = []
     DNA = readIn()
     Hdist = 0
     y = 0
     for x in range(len(DNA)):
         CompareArr.append(compDNA(DNA[x]))
-
     for x in range(len(CompareArr[y])):
         if CompareArr[y][x] != CompareArr[y+1][x]:
             Hdist += 1
