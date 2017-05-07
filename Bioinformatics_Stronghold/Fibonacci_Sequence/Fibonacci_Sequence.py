d943c04bf05c8464fe9eb4a8c502832e85e877e6
iaji <ajcardar@gmail.com>
2017-05-07 19:22:02 -0400

    Added docstrings

--- Bioinformatics_Stronghold/Fibonacci_Sequence/Fibonacci_Sequence.py
+++ Bioinformatics_Stronghold/Fibonacci_Sequence/Fibonacci_Sequence.py
@@ -1,12 +1,14 @@
 
 def fib_rabbits(n,k):
+    """ Reads in file for n as in number of months and k as in number of babies per split. """
     rabbits = [0,1]
     for i in xrange(n-1):
         rabbits[i % 2] = rabbits[(i-1) % 2] + k*rabbits[i % 2]
 
     return rabbits[n % 2]
 
-with open('rosalind_fib.txt') as inputs:
+with open('rosalind_fib.txt', 'r') as inputs:
     n, k = map(int, inputs.read().strip().split())
+
 
 print fib_rabbits(n,k)
